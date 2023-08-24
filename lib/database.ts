e385ba7a0b2804229f752194b19ce6fdf17b4525
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 21:37:12 +0200

    fixed mongodb connection

--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,3 @@
-import Payer from "@lib/models/payer.model";
 import mongoose from "mongoose";
 
 let isConnected = false;
@@ -7,12 +6,13 @@ export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found!");
-  if (isConnected) return console.log("MongoDB is already connected!");
-  try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    isConnected = true;
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.log(error);
+  if (!isConnected) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URL);
+      isConnected = true;
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
