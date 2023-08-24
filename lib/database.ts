bfae06ff1d315f6b0ec2f71f71f78121b2ba3c01
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 22:08:37 +0200

    change database connection

--- lib/database.ts
+++ lib/database.ts
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
+  // Set strict query mode for Mongoose to prevent unknown field queries.
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found!");
-  if (!isConnected) {
-    try {
-      await mongoose.connect(process.env.MONGODB_URL);
-      isConnected = true;
-      console.log("Connected to MongoDB");
-    } catch (error: any) {
-      console.log(`Failed to connect to mongodb ${error.message}`);
-    }
-  } else {
-    console.log("MongoDB is already connected");
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true; // Set the connection status to true
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
   }
 };
