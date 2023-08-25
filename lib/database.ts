f4bdb76fc8e4f5666321be8a307be8e45f1fc8fa
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 10:40:35 +0200

    Try another way of connecting to database (9)

--- lib/database.ts
+++ lib/database.ts
@@ -1,21 +1,27 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectToDB = async () => {
-  if (!process.env.MONGODB_URL) {
-    console.log("Missing MongoDB URL");
-    return;
-  }
-
-  try {
-    if (mongoose.connection.readyState !== 1) {
-      // Check if not already connected
-      await mongoose.connect(process.env.MONGODB_URL);
-
-      console.log("MongoDB connected3");
-    } else {
-      console.log("MongoDB connection already established333");
+  if (!isConnected) {
+    if (!process.env.MONGODB_URL) {
+      console.log("Missing MongoDB URL");
+      return;
     }
-  } catch (error) {
-    console.log(error);
+
+    try {
+      if (mongoose.connection.readyState !== 1) {
+        // Check if not already connected
+        await mongoose.connect(process.env.MONGODB_URL);
+        isConnected = true;
+        console.log("MongoDB connected3");
+      } else {
+        console.log("MongoDB connection already established333");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("MongoDB connection already established444");
   }
 };
--- pages/api/transactionResponse.ts
+++ pages/api/transactionResponse.ts
@@ -1,5 +1,6 @@
 import { createOrder, grantOrderItem } from "@lib/actions/order.actions";
 import { fetchPayer } from "@lib/actions/payer.actions";
+import { connectToDB } from "@lib/database";
 
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -11,20 +12,27 @@ export default async function handler(
     if (req.body["tr_status"] === "TRUE") {
       const hiddenDescription = JSON.parse(req.body["tr_crc"]);
 
-      const payer = await fetchPayer(
-        hiddenDescription.nick,
-        hiddenDescription.email,
-      );
+      //   const payer = await fetchPayer(
+      //     hiddenDescription.nick,
+      //     hiddenDescription.email,
+      //   );
 
-      const order = await createOrder({
-        name: hiddenDescription.item_name,
-        days: hiddenDescription.days,
-        price: hiddenDescription.price,
-        payerId: payer._id,
-      });
-      await grantOrderItem(order._id);
+      //   const order = await createOrder({
+      //     name: hiddenDescription.item_name,
+      //     days: hiddenDescription.days,
+      //     price: hiddenDescription.price,
+      //     payerId: payer._id,
+      //   });
+      //   await grantOrderItem(order._id);
+      // }
+      console.log("0");
+      await connectToDB();
+      console.log("1");
+      await connectToDB();
+      console.log("2");
+      await connectToDB();
+      console.log("3");
     }
-
     res.status(200).send("TRUE");
   } catch (e) {
     res.status(501).send({ FALSE: e });
