3390ce118d34d9b2e40063e102813a51d581447c
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 12:08:56 +0200

    Try another way of connecting to database (21)

--- lib/database.ts
+++ lib/database.ts
@@ -62,23 +62,23 @@ export async function grantOrderItem(orderId: string) {
     ) {
       throw new Error("Invalid rcon data");
     }
-    const rcon = await Rcon.connect({
-      host: process.env.RCON_HOST,
-      password: process.env.RCON_PASSWORD,
-      port: parseInt(process.env.RCON_PORT),
-    });
+    // const rcon = await Rcon.connect({
+    //   host: process.env.RCON_HOST,
+    //   password: process.env.RCON_PASSWORD,
+    //   port: parseInt(process.env.RCON_PORT),
+    // });
 
-    const response = await rcon.send(
-      `grant ${order.payer.nick} ${String(order.name).replaceAll(" ", "_")} ${
-        order.days
-      }`,
-    );
-    rcon.end();
-    if (response === "Success") {
-      await Order.findByIdAndUpdate(orderId, {
-        $set: { granted: true },
-      });
-    }
+    // const response = await rcon.send(
+    //   `grant ${order.payer.nick} ${String(order.name).replaceAll(" ", "_")} ${
+    //     order.days
+    //   }`,
+    // );
+    // rcon.end();
+    // if (response === "Success") {
+    //   await Order.findByIdAndUpdate(orderId, {
+    //     $set: { granted: true },
+    //   });
+    // }
   } catch (error: any) {
     throw new Error(`Failed to grant order's item ${error.message}`);
   }
