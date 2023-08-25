64a661229e02483af99e6aff602bd9e06e308b1a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 11:50:05 +0200

    Try another way of connecting to database (18)

--- lib/database.ts
+++ lib/database.ts
@@ -11,14 +11,9 @@ export const connectToDB = async () => {
       return;
     }
     try {
-      if (mongoose.connection.readyState !== 1) {
-        // Check if not already connected
-        await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true;
-        console.log("MongoDB connected3");
-      } else {
-        console.log("MongoDB connection already established333");
-      }
+      await mongoose.connect(process.env.MONGODB_URL);
+      isConnected = true;
+      console.log("MongoDB connected3");
     } catch (error) {
       console.log(error);
     }
