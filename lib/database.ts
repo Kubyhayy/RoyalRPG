492b25e60c014038560f9757d33ad2c6322052cd
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 09:57:21 +0200

    Try another way of connecting to database (1)

--- lib/database.ts
+++ lib/database.ts
@@ -1,24 +1,19 @@
 import mongoose from "mongoose";
 
-let isConnected = false; // Variable to track the connection status
-
 export const connectToDB = async () => {
-  // Set strict query mode for Mongoose to prevent unknown field queries.
-  mongoose.set("strictQuery", true);
-
-  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
-
-  // If the connection is already established, return without creating a new connection.
-  if (isConnected) {
-    console.log("MongoDB connection already established");
+  if (!process.env.MONGODB_URL) {
+    console.log("Missing MongoDB URL");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
-
-    isConnected = true; // Set the connection status to true
-    console.log("MongoDB connected");
+    if (mongoose.connection.readyState !== 1) {
+      // Check if not already connected
+      await mongoose.connect(process.env.MONGODB_URL);
+      console.log("MongoDB connected");
+    } else {
+      console.log("MongoDB connection already established");
+    }
   } catch (error) {
     console.log(error);
   }
